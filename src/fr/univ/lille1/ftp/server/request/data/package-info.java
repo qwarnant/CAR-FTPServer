2377b0ce8510e31931a74a0fbef8a82ebb8890b9
Quentin Warnant <qwarnant@gmail.com>
2015-02-17 12:28:59 +0100

    default data dir

---
+++ src/fr/univ/lille1/ftp/server/request/data/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This package contains all the classes for the ftp requests which
+ * requires a data connection between the client and the server
+ *
+ * @author Quentin Warnant
+ * @version 1.0
+ */
+package fr.univ.lille1.ftp.server.request.data;
